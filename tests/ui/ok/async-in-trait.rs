1b9699bb7244b1290eb66a9ff6b1b77460080c4f
Andy Lok <andylokandy@hotmail.com>
2023-07-23 05:25:51 +0800

    fix test in stable

--- tests/ui/ok/async-in-trait.rs
+++ tests/ui/ok/async-in-trait.rs
@@ -1,17 +1,19 @@
-#![feature(async_fn_in_trait)]
-#![allow(unused_mut)]
+#![cfg_attr(all(feature = "nightly", test), feature(async_fn_in_trait))]
 
-trait MyTrait {
-    async fn work(&self) -> Result<usize, usize>;
-}
-
-struct MyStruct;
-
-impl MyTrait for MyStruct {
-    #[logcall::logcall("debug")]
-    #[logcall::logcall(ok = "debug", err = "error")]
-    async fn work(&self) -> Result<usize, usize> {
-        Ok(1)
+#[cfg(all(feature = "nightly", test))]
+mod tests {
+    trait MyTrait {
+        async fn work(&self) -> Result<usize, usize>;
+    }
+    
+    struct MyStruct;
+    
+    impl MyTrait for MyStruct {
+        #[logcall::logcall("debug")]
+        #[logcall::logcall(ok = "debug", err = "error")]
+        async fn work(&self) -> Result<usize, usize> {
+            Ok(1)
+        }
     }
 }
 
